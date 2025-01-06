953d2adfa1206a622ea4609a930853f8d4258805
Hannah Lu <64236360+hannahlu1992@users.noreply.github.com>
2025-01-06 11:44:34 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+//This is single-line comment, it ends at the end of the line.
+/*This is multiline comment,
+and it extends until the closing star-slash comment mark.*/
+/*We like to write comments at the top of each function
+to explain what the function does
+and what values it returns*/
+// This function prints Hello, world!
+int main(){
+    cout << "Hello, world!" << endl;
+    return 0;
+}
+
+
+
+
